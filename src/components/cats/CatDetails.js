54ac24071ffa6b3d44c6dcbdd5884f2170e9537b
Katharine Zarbock <kjzarbock@gmail.com>
2023-09-16 13:01:20 -0500

    9/17 1921, delete moved directly to cat profile

--- src/components/cats/CatDetails.js
+++ src/components/cats/CatDetails.js
@@ -1,16 +1,16 @@
 import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
 import { Link } from 'react-router-dom';
-import { getSingleCat, updateCat } from '../../managers/CatManager'; // Import the updateCat function
+import { getSingleCat, updateCat, deleteCat } from '../../managers/CatManager';
 import { Background } from '../background/Background';
 import { EditCat } from './EditCat';
-import { DeleteCat } from './DeleteCat';
 
 export const CatDetails = ({ catId: catIdProp }) => {
   const [cat, setCat] = useState({});
   const { catId: catIdParam } = useParams();
   const catId = catIdProp || catIdParam;
-  const user = JSON.parse(localStorage.getItem("kitty_user")); // Retrieve user info from local storage
+  const user = JSON.parse(localStorage.getItem('kitty_user'));
+  const navigate = useNavigate();
 
   useEffect(() => {
     getSingleCat(catId).then((fetchedCat) => {
@@ -18,19 +18,14 @@ export const CatDetails = ({ catId: catIdProp }) => {
     });
   }, [catId]);
 
-  // Define a state to control the visibility of the EditCat and DeleteCat components
   const [isEditing, setIsEditing] = useState(false);
-  const [isDeleting, setIsDeleting] = useState(false);
 
-  // Function to update the cat details and refresh
   const handleCatUpdate = (updatedCat) => {
     updateCat(cat.id, updatedCat)
       .then((response) => {
         if (response !== null) {
-          // Cat updated successfully, you can handle success here
-          console.log('Cat updated:', response);
-          setCat(updatedCat); // Update the cat details in the component state
-          setIsEditing(false); // Close the edit form
+          setCat(updatedCat);
+          setIsEditing(false);
         } else {
           console.error('Failed to update cat.');
         }
@@ -40,43 +35,48 @@ export const CatDetails = ({ catId: catIdProp }) => {
       });
   };
 
+  const handleDelete = () => {
+    if (window.confirm('Are you sure you want to delete this cat?')) {
+      deleteCat(cat.id)
+        .then(() => {
+          navigate('/cats');
+        })
+        .catch((error) => {
+          console.error('Error deleting cat:', error);
+        });
+    }
+  };
+
   return (
     <>
-      <section className="post">
-        <div className="user-list-name">Name: {cat.name}</div>
+      <section className='post'>
+        <div className='user-list-name'>Name: {cat.name}</div>
         <img src={cat.image} alt={cat.name} />
-        <div className="cat-list-location">
+        <div className='cat-list-location'>
           Location: {cat.location && <Link to={`/locations/${cat.location.id}`}>{cat.location.name}</Link>}
         </div>
-        <div className="cat-list-name">Age: {cat.age}</div>
-        <div className="cat-list-name">Sex: {cat.sex}</div>
-        <div className="cat-list-name">Bio: {cat.bio}</div>
-        <div className="cat-list-name">Adopted: {cat.adopted ? 'Yes' : 'No'}</div>
+        <div className='cat-list-name'>Age: {cat.age}</div>
+        <div className='cat-list-name'>Sex: {cat.sex}</div>
+        <div className='cat-list-name'>Bio: {cat.bio}</div>
+        <div className='cat-list-name'>Adopted: {cat.adopted ? 'Yes' : 'No'}</div>
         <div>Gets along with cats: {cat.gets_along_with_cats ? 'Yes' : 'No'}</div>
         <div>Gets along with dogs: {cat.gets_along_with_dogs ? 'Yes' : 'No'}</div>
         <div>Gets along with children: {cat.gets_along_with_children ? 'Yes' : 'No'}</div>
-
-        {/* Show Edit and Delete buttons only to staff members */}
+        
         {user && user.staff && (
           <div>
             <button onClick={() => setIsEditing(true)}>Edit Cat</button>
-            <button onClick={() => setIsDeleting(true)}>Delete Cat</button>
+            <button onClick={handleDelete}>Delete Cat</button>
           </div>
         )}
-
-        <Link to={`/cats`} className="cat-profile-button">
-          Back to all Cats
-        </Link>
+        
+        <Link to={`/cats`} className='cat-profile-button'>Back to all Cats</Link>
       </section>
-
-      {/* Render EditCat component if isEditing state is true */}
+      
       {isEditing && (
         <EditCat cat={cat} onClose={() => setIsEditing(false)} onUpdate={handleCatUpdate} />
       )}
-
-      {/* Render DeleteCat component if isDeleting state is true */}
-      {isDeleting && <DeleteCat cat={cat} onClose={() => setIsDeleting(false)} />}
-
+      
       <Background />
     </>
   );
